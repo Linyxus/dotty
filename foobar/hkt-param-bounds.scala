59b64d5a138de36e9d022df9fb7777d5a4b1357d
Yichen Xu <yichen.x@outlook.com>
2021-03-24 15:02:12 +0800

    Update HKT parameter bounds example

--- foobar/hkt-param-bounds.scala
+++ foobar/hkt-param-bounds.scala
@@ -1,6 +1,7 @@
 object test {
   final class HKTVar[+F[_]]
   final class HKTVarVar[+F[_], +M[_]]
+  final class HKTInv[F[_]]
 
   // def foo[G[_], F[x] <: G[x], X](fx : F[X]) = {
   //   val gx : G[X] = fx
@@ -11,8 +12,13 @@ object test {
   //     val gx : G[X] = fx
   // }
 
-  def baz[F[_], G[_], H[_], X](x : HKTVarVar[G, H], fx : F[X]) = x match {
-    case _ : HKTVarVar[F, F] =>
+  // def baz[F[_], G[_], H[_], X](x : HKTVarVar[G, H], fx : F[X]) = x match {
+  //   case _ : HKTVarVar[F, F] =>
+  //     val gx : G[X] = fx
+  // }
+
+  def baaz[F[_], G[_], X](x : HKTInv[G], fx : F[X]) = x match {
+    case _ : HKTInv[F] =>
       val gx : G[X] = fx
   }
 
